7a14ffbf19e538c785394cf1d5c5ffc18c97f704
Sergey Lebedev <londeren@gmail.com>
2013-10-22 11:47:01 +0400

    remove component namespace

--- Components/super.component.complex/templates/.default/vidicom/super.component/sections/result_modifier.php
+++
@@ -1,5 +0,0 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-
-// component text here
-
-?>
--- Components/super.component.complex/templates/.default/vidicom/super.component/sections/template.php
+++
@@ -1,1 +0,0 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
